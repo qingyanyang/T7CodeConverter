ab4e6d7f606c8ffa497065a45795120b22ab1edf
a1865304 <yangqingyan0@gmail.com>
2023-11-14 20:54:58 +1030

    try

--- code_converter/settings.py
+++ code_converter/settings.py
@@ -117,17 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     "/var/www/static/",
+#     BASE_DIR / "static"
 # ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
